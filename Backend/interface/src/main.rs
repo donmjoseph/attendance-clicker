6b23be03a1f310d112923d944b615fcea5c2c63b
Logan Meyers <theterrarian03@gmail.com>
2024-10-27 12:10:58 -0700

    Began working on a database

--- Backend/interface/src/main.rs
+++ Backend/interface/src/main.rs
@@ -1,3 +1,5 @@
+mod schema;
+
 use axum::{routing::get, Router};
 
 #[tokio::main]
@@ -9,11 +11,14 @@ async fn main() {
         .route("/ea-sports", get(in_the_game)); // i have brainrot
 
     // gets a listener for port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+    let listener = tokio::net::TcpListener::bind("localhost:3000")
         .await
         .unwrap();
 
-    axum::serve(listener, app).await.unwrap();
+    println!("Starting backend...");
+
+    // bind the address and start the server
+    axum::serve(listener, app.into_make_service()).await.unwrap();
 }
 
 async fn handle_hello() -> String {
