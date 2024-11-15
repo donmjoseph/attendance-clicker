24722648aad863f17ded0bb658bbe3cba3e68724
Logan Meyers <theterrarian03@gmail.com>
2024-11-14 16:02:33 -0800

    Started making handlers, removed silly (though funny) routes made for testing, and started getting an idea of how things work with databases.

--- Backend/interface/src/main.rs
+++ Backend/interface/src/main.rs
@@ -1,3 +1,4 @@
+mod handler;
 mod schema;
 
 use axum::{routing::get, Router};
@@ -12,30 +13,18 @@ async fn main() {
 
     // creates a handler that routes 
     let app = Router::new()
-        .route("/", get(handle_hello))
-        .route("/database_url", get(database_url))
-        .route("/goodbye", get(handle_goodbye))
-        .route("/ea-sports", get(in_the_game)); // i have brainrot
+        .route("/", get(handler::health_check_handler))
+        .route("/database_url", get(database_url));
 
     // gets a listener for port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
         .unwrap();
 
-    println!("Starting backend... once more");
+    println!("Starting backend...");
 
     // bind the address and start the server
     axum::serve(listener, app.into_make_service()).await.unwrap();
-}
 
-async fn handle_hello() -> String {
-    "Hello, World!!!!!!".to_string()
-}
-
-async fn handle_goodbye() ->  String {
-    "Goodbye, World!".to_string()
-}
-
-async fn in_the_game() -> String {
-    "It's in the game".to_string()
+    println!("Backend started!");
 }
