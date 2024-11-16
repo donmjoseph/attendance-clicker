6efc4c95385723e2b20a3bb98ce4db100aaa54e9
Don <128012789+donmjoseph@users.noreply.github.com>
2024-11-16 15:31:17 -0800

    Updated CheckedIn component and styling

---
+++ Client/bulma_project/src/app/CheckedIn/page.tsx
@@ -0,0 +1,33 @@
+'use client';
+import React from 'react';
+import { useRouter } from "next/navigation";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import styles from './CheckedIn.module.css'; // Importing CSS module
+
+export default function CheckedIn() {
+
+    const router = useRouter();
+
+    const handleBackToClasses = () => {
+        router.push('/');
+    }
+
+    return (
+        <div className={styles.checkedInBox}>
+            <div className={styles.header}>
+                <h2>CHEM 105 Section 02</h2>
+            </div>
+            <div className={styles.content}>
+                <FontAwesomeIcon icon={faCheckCircle} size="4x" className={styles.icon} />
+                <h3 className={styles.title}>You’re checked in!</h3>
+                <p className={styles.message}>
+                    Stay on this screen to remain in class. We’ll let you know when your instructor starts an activity.
+                </p>
+            </div>
+            <button className={styles.backButton} onClick={handleBackToClasses}>
+                Back to Classes
+            </button>
+        </div>
+    );
+}
