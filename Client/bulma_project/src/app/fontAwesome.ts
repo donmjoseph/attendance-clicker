b934348c76065c749bd1e8cfc36f1f8164d72936
Johnny <chanky789@icloud.com>
2024-11-16 14:09:06 -0800

    Big push

---
+++ Client/bulma_project/src/app/fontAwesome.ts
@@ -0,0 +1,6 @@
+// src/fontawesome.ts
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+// Add all solid icons to the library (you can be selective here)
+library.add(fas);
---
+++ Client/bulma_project/src/app/pages/_app.tsx
@@ -0,0 +1,12 @@
+import '../app/globals.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css'; 
+config.autoAddCss = false; 
+import '@/fontawesome';
+import { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
