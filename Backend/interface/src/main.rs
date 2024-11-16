a9bc8616c8d741d37fdfa24383222fd7f5a8b2fc
Logan Meyers <theterrarian03@gmail.com>
2024-11-15 20:36:18 -0800

    Started making connection to database, added a makefile with commands for convenience, and git status currently it does not build successfully git status

--- Backend/interface/src/main.rs
+++ Backend/interface/src/main.rs
@@ -1,6 +1,7 @@
 mod handler;
 mod schema;
 
+use sqlx::postgres::PgPoolOptions;  // Pool, Postgres
 use axum::{routing::get, Router};
 use dotenv::dotenv;
 
@@ -11,10 +12,18 @@ async fn main() {
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     println!("database url: {}", database_url);
 
+    // Create a connection pool
+    let pool = PgPoolOptions::new()
+        .max_connections(5) // Adjust based on your needs
+        .connect(&database_url)
+        .await
+        .expect("Failed to create database pool");
+
     // creates a handler that routes 
     let app = Router::new()
         .route("/", get(handler::health_check_handler))
-        .route("/database_url", get(database_url));
+        .route("/database_url", get(database_url))
+        .with_state(pool);
 
     // gets a listener for port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
@@ -25,6 +34,4 @@ async fn main() {
 
     // bind the address and start the server
     axum::serve(listener, app.into_make_service()).await.unwrap();
-
-    println!("Backend started!");
 }
